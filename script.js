60035133b537a9bcd75fee624d2f1226c36daddf
daFabii <fabian.brandstetter02@gmail.com>
2025-10-10 17:05:05 +0200

    implemented shop, buying freaky clickers now possible

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 class Clicker{
     clickCount = 0;
-    clicksPerSecond = 0;
+    clicksPerSecond = 1;
+
+    freakyClickerPrice = 30;
+    freakyClickerAmount = 0;
+
     secondsPlayed = 0;
 
-    increaseClickPerSecond(){
-        if(this.clickCount % 20 == 0){
-            this.clicksPerSecond++;
-        }
+    increaseClicksPerSecond(amount, price){
+            this.clicksPerSecond += amount;
+            this.clickCount -= price;
     }
     increaseClickCount(){
         for(let i = 0; i < this.clicksPerSecond; i++){
             this.clickCount += 1;
-            this.increaseClickPerSecond();
         }
 
         updateStats();
+    }
+    buyFreakyClicker(amount){
+        if(this.clickCount >= this.freakyClickerPrice){
+            this.increaseClicksPerSecond(amount, this.freakyClickerPrice);
+            this.freakyClickerPrice += 10;
+            this.freakyClickerAmount++;
+            document.getElementById("btn_buyFreakyClicker").innerHTML = this.freakyClickerPrice + " clicks";
+            document.getElementById("div_freakyClickerAmount").innerHTML = this.freakyClickerAmount + "x";
+        }
     }
     updateStats(){
         document.getElementById("div_clicksPerSecond").innerHTML = this.clicksPerSecond.toLocaleString('de-DE');
@@ -22,7 +33,6 @@ class Clicker{
         document.getElementById("div_secondsPlayed").innerHTML = this.secondsPlayed;
     }
     startTimer() {
-        this.increaseClickPerSecond();
         setInterval(() => {
             this.secondsPlayed++;
             this.increaseClickCount();
@@ -33,6 +43,9 @@ class Clicker{
 const clicker = new Clicker();
 init();
 
+function buyFreakyClicker(amount){
+    clicker.buyFreakyClicker(amount);
+}
 function countUp(){
     clicker.increaseClickCount();
 }
